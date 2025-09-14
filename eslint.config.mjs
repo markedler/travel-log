571f4a2a172f48dbbdd5185cd838bd0dfa07c791
Mark Edler <markedler@gmail.com>
2025-09-14 23:50:10 +1000

    :sparkles: setup eslint / lint-staged (#48)

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import antfu from "@antfu/eslint-config";
+
+import withNuxt from "./.nuxt/eslint.config.mjs";
+
+export default withNuxt(antfu({
+  type: "app",
+  typescript: true,
+  formatters: true,
+  stylistic: {
+    indent: 2,
+    semi: true,
+    quotes: "double",
+  },
+  ignores: ["**/migrations/*", ".pnpm-store/**"],
+}, {
+  rules: {
+    "no-console": ["warn"],
+    "antfu/no-top-level-await": ["off"],
+    "node/prefer-global/process": ["off"],
+    "node/no-process-env": ["error"],
+    "perfectionist/sort-imports": ["error", {
+      tsconfigRootDir: ".",
+    }],
+    "unicorn/filename-case": ["error", {
+      case: "kebabCase",
+      ignore: ["README.md"],
+    }],
+    "unused-imports/no-unused-imports": ["warn"],
+  },
+}));
