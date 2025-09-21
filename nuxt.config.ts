f78b875003a7f2a37297a131b65f737a091a2406
Mark Edler <mark.edler@acurus.com.au>
2025-09-21 23:23:03 +1000

    :sparkles: add color mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,19 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/icon"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/icon",
+    "@nuxtjs/color-mode",
+  ],
   css: ["~/assets/css/main.css"],
   vite: {
     plugins: [
       tailwindcss(),
     ],
+  },
+  colorMode: {
+    dataValue: "theme",
   },
   eslint: {
     config: {
